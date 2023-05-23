068d849bb961c291f5bbfe4265be8020f55d565a
kevinMartinezC <ceronk003@gmail.com>
2023-05-23 14:35:13 -0600

    feat: add ResultCard and SearchResultLists previes

--- app/src/main/java/com/example/musicplayer/ui/component/home/SearchResultLists.kt
+++ app/src/main/java/com/example/musicplayer/ui/component/home/SearchResultLists.kt
@@ -4,17 +4,28 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.domain.model.Images
+import com.example.domain.model.Previews
 import com.example.domain.model.SoundResult
+import com.example.musicplayer.R
+import com.example.musicplayer.ui.theme.MusicPlayerTheme
+import kotlinx.coroutines.flow.flowOf
 
 private const val GRID_CELLS_SIZE = 4
+
 @Composable
 fun SearchResultsGrid(results: LazyPagingItems<SoundResult>, navController: NavHostController) {
     LazyVerticalGrid(columns = GridCells.Fixed(GRID_CELLS_SIZE)) {
         items(results.itemCount) { index ->
             val animeItem = results[index] ?: return@items
-            ResultCard(musicItem = animeItem, grid = true,  navController = navController)
+            ResultCard(musicItem = animeItem, grid = true, navController = navController)
         }
     }
 }
@@ -24,7 +35,70 @@ fun SearchResultsColumn(results: LazyPagingItems<SoundResult>, navController: Na
     LazyColumn {
         items(results.itemCount) { result ->
             val animeItem = results[result] ?: return@items
-            ResultCard(musicItem = animeItem, grid = false,  navController = navController)
+            ResultCard(musicItem = animeItem, grid = false, navController = navController)
         }
     }
+}
+
+@Composable
+@Preview
+private fun SearchResultsGridPreview() {
+    val fakeResults = flowOf(
+        PagingData.from(
+            listOf(
+                SoundResult(
+                    id = 1,
+                    name = stringResource(R.string.name),
+                    username = stringResource(R.string.user_name),
+                    previews = Previews(previewHqMp3 = stringResource(R.string.music_mp3)),
+                    images = Images(waveformM = stringResource(R.string.image))
+                ),
+                SoundResult(
+                    id = 1,
+                    name = stringResource(R.string.name),
+                    username = stringResource(R.string.user_name),
+                    previews = Previews(previewHqMp3 = stringResource(R.string.music_mp3)),
+                    images = Images(waveformM = stringResource(R.string.image))
+                ),
+                SoundResult(
+                    id = 1,
+                    name = stringResource(R.string.name),
+                    username = stringResource(R.string.user_name),
+                    previews = Previews(previewHqMp3 = stringResource(R.string.music_mp3)),
+                    images = Images(waveformM = stringResource(R.string.image))
+                ),
+                SoundResult(
+                    id = 1,
+                    name = stringResource(R.string.name),
+                    username = stringResource(R.string.user_name),
+                    previews = Previews(previewHqMp3 = stringResource(R.string.music_mp3)),
+                    images = Images(waveformM = stringResource(R.string.image))
+                )
+            )
+        )
+    ).collectAsLazyPagingItems()
+
+    MusicPlayerTheme {
+        SearchResultsGrid(results = fakeResults, navController = rememberNavController())
+    }
+}
+
+@Composable
+@Preview
+private fun SearchResultsColumnPreview() {
+    val fakeResults = flowOf(
+        PagingData.from(
+            listOf(
+                SoundResult(
+                    id = 1,
+                    name = stringResource(R.string.name),
+                    username = stringResource(R.string.user_name),
+                    previews = Previews(previewHqMp3 = stringResource(R.string.music_mp3)),
+                    images = Images(waveformM = stringResource(R.string.image))
+                )
+            )
+        )
+    ).collectAsLazyPagingItems()
+
+    SearchResultsColumn(results = fakeResults, navController = rememberNavController())
 }
