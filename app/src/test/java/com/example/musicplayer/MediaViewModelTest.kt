4c12309c7ca84f6823b4c6ebc5319ddb1e0c52a3
kevinMartinezC <ceronk003@gmail.com>
2023-05-31 11:47:52 -0600

    feat: add UnitTest for the onUIEvent fun to the events Backward, Forward and PlayPause

--- app/src/test/java/com/example/musicplayer/MediaViewModelTest.kt
+++ app/src/test/java/com/example/musicplayer/MediaViewModelTest.kt
@@ -5,6 +5,7 @@ import com.example.data.service.media.MediaServiceHandler
 import com.example.data.service.media.utils.MediaState
 import com.example.data.service.media.utils.PlayerEvent
 import com.example.domain.usecases.LoadSongUseCase
+import com.example.musicplayer.ui.component.player.utils.UIEvent
 import com.example.musicplayer.ui.component.player.viewmodel.MediaViewModel
 import io.mockk.Runs
 import io.mockk.coEvery
@@ -25,40 +26,37 @@ class MediaViewModelTest {
 
     @get:Rule
     var mainCoroutineRule = MainCoroutineRule()
+
     @Test
     fun collectMediaState_emitsMediaState_changesUiStatePlayer() = runTest {
-        // Arrange
+
         val mediaStateFlow = MutableStateFlow<MediaState>(MediaState.Initial)
         coEvery { mockMediaServiceHandler.mediaState } returns mediaStateFlow
 
         val viewModel = MediaViewModel(mockMediaServiceHandler, mockLoadSongUseCase)
 
-        // Act
         mediaStateFlow.value = MediaState.Playing(true)
 
-        // Assert
         assertEquals(true, viewModel.uiStatePlayer.value.isPlaying)
     }
 
     @Test
     fun collectMediaState_emitsMediaStateReady_changesUiStatePlayerDuration() = runTest {
-        // Arrange
+
         val mediaStateFlow = MutableStateFlow<MediaState>(MediaState.Initial)
         coEvery { mockMediaServiceHandler.mediaState } returns mediaStateFlow
 
         val viewModel = MediaViewModel(mockMediaServiceHandler, mockLoadSongUseCase)
 
-        // Act
         val duration = 100L
         mediaStateFlow.value = MediaState.Ready(duration)
 
-        // Assert
         assertEquals(duration, viewModel.uiStatePlayer.value.duration)
     }
 
     @Test
     fun onCleared_callsOnPlayerEventWithStop() = runTest{
-        // Arrange
+
         val mediaStateFlow = MutableStateFlow<MediaState>(MediaState.Initial)
         coEvery { mockMediaServiceHandler.mediaState } returns mediaStateFlow
         coEvery { mockMediaServiceHandler.onPlayerEvent(PlayerEvent.Stop) } just Runs
@@ -68,6 +66,51 @@ class MediaViewModelTest {
         viewModel.onCleared()
 
         coVerify { mockMediaServiceHandler.onPlayerEvent(PlayerEvent.Stop) }
+    }
+
+    @Test
+    fun onUIEvent_whenBackward_callsOnPlayerEventWithBackward() = runTest {
+
+        val mediaStateFlow = MutableStateFlow<MediaState>(MediaState.Initial)
+        coEvery { mockMediaServiceHandler.mediaState } returns mediaStateFlow
+        coEvery { mockMediaServiceHandler.onPlayerEvent(PlayerEvent.Backward) } just Runs
+
+        val viewModel = MediaViewModel(mockMediaServiceHandler, mockLoadSongUseCase)
+
+        viewModel.onUIEvent(UIEvent.Backward)
+
+        coVerify { mockMediaServiceHandler.onPlayerEvent(PlayerEvent.Backward) }
+
+    }
+
+    @Test
+    fun onUIEvent_whenForward_callsOnPlayerEventWithForward() = runTest {
+
+        val mediaStateFlow = MutableStateFlow<MediaState>(MediaState.Initial)
+        coEvery { mockMediaServiceHandler.mediaState } returns mediaStateFlow
+        coEvery { mockMediaServiceHandler.onPlayerEvent(PlayerEvent.Forward) } just Runs
+
+        val viewModel = MediaViewModel(mockMediaServiceHandler, mockLoadSongUseCase)
+
+        viewModel.onUIEvent(UIEvent.Forward)
+
+        coVerify { mockMediaServiceHandler.onPlayerEvent(PlayerEvent.Forward) }
+
+    }
+
+    @Test
+    fun onUIEvent_whenPlayPause_callsOnPlayerEventWithPlayPause() = runTest {
+
+        val mediaStateFlow = MutableStateFlow<MediaState>(MediaState.Initial)
+        coEvery { mockMediaServiceHandler.mediaState } returns mediaStateFlow
+        coEvery { mockMediaServiceHandler.onPlayerEvent(PlayerEvent.PlayPause) } just Runs
+
+        val viewModel = MediaViewModel(mockMediaServiceHandler, mockLoadSongUseCase)
+
+        viewModel.onUIEvent(UIEvent.PlayPause)
+
+        coVerify { mockMediaServiceHandler.onPlayerEvent(PlayerEvent.PlayPause) }
+
     }
 
 }
